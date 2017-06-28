ef83b127f8b6c84990aae1dc257e2640f6ac05cf
Charles Szilagyi <charles@charlesagile.com>
2017-06-28 10:14:38 +0100

    Open spec file in column 2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 # Change Log
 
-0.0.1: Initial release that can open a .spec.* file
+* 0.0.1: Initial release that can open a .spec.* file
+* 0.0.2: Open spec file in 2nd column
--- src/openFileInEditor.js
+++ src/openFileInEditor.js
@@ -3,7 +3,7 @@ var vscode = require('vscode')
 module.exports = async function(filePath) {
     try {
         var document = await vscode.workspace.openTextDocument(filePath)
-        await vscode.window.showTextDocument(document)
+        await vscode.window.showTextDocument(document, 2)
     } catch(error) {
         vscode.window.showErrorMessage('Unable to open ' + filePath)
 
