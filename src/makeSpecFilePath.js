ff79b246667e44b5a551e34c8fbfa0f24ff7d06c
Charles Szilagyi <charles@charlesagile.com>
2017-07-25 18:17:34 +0100

    Allow configuring the suffix via VSCode's preferences

--- src/makeSpecFilePath.js
+++ src/makeSpecFilePath.js
@@ -1,11 +1,14 @@
 var path = require('path')
 
-var SPEC_SUFFIX = '.spec'
+var DEFAULT_SPEC_SUFFIX = '.spec'
+
+var configuration = vscode.workspace.getConfiguration('myExtension')
+var specSuffix = configuration.get('specSuffix') || DEFAULT_SPEC_SUFFIX
 
 module.exports = function(srcFile) {
     var srcFilePath = srcFile.uri.fsPath
     var parts = path.parse(srcFilePath)
-    var specFileName = parts.name + SPEC_SUFFIX + parts.ext
+    var specFileName = parts.name + specSuffix + parts.ext
 
     return path.join(parts.root, parts.dir, specFileName)
 }
