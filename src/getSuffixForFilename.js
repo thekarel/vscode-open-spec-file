8db9f9fe619d4b687f870fd2b62c5fb1933be979
Nate Taylor <taylonr@gmail.com>
2017-09-09 16:33:17 -0500

    Revert "Use the longest key as the 'best' match"
    This reverts commit 7ac3aba7be5d927ad4a397da4b172adc3e50c59a.

--- src/getSuffixForFilename.js
+++ src/getSuffixForFilename.js
@@ -1,22 +1,17 @@
-const isSuffixMatch = str => suffix => new RegExp(`${suffix}$`).test(str)
-const byLengthDescending = (first, second) => second.length - first.length;
+const isSuffixMatch = (suffix, str) => new RegExp(`${suffix}$`).test(str)
 
 const NO_MATCH = {
     toSuffix: null
 }
 
 module.exports = (map, fileName) => {
-    const keys = Object.keys(map);
+    for (const fromSuffix in map) {
 
-    const match = keys
-    .filter(isSuffixMatch(fileName))
-    .sort(byLengthDescending)
-    .shift();
-
-    if(match){
-        return {
-            fromSuffix: match,
-            toSuffix: map[match]
+        if (isSuffixMatch(fromSuffix, fileName)) {
+            return {
+                fromSuffix,
+                toSuffix: map[fromSuffix]
+            }
         }
     }
 
